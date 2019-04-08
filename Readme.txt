43365ba8f01740b8f72db185ba58a8974abaac67
twoh4nds <twohands@daum.net>
2019-04-08 17:03:17 +0900

    2019.04.08

--- Readme.txt
+++ Readme.txt
@@ -1,33 +1,34 @@
 
 
 
-
+개발환경 및 사용 툴
 
 Subject
  - Web Crawling
 
 Front
- - Vue.js
+ - Vue.js(FrontEnd FrameWork)
 
-Back(Library)
- - JAVA(JSoup)/파이썬(BeautifulSoup)
+Back[Library]
+ - JAVA[JSoup] - Spring Boot(BackEnd FrameWork)/파이썬[BeautifulSoup] - (BackEnd FrameWork)
 
 DB
- - Oracle/MongoDB/MySQL
+ - MongoDB
 
-IssueTracker
- - 미정(https://jm4488.tistory.com/29)
+가상컨테이너
+ - Docker
+
+Server
+ - Tomcat
+
+IssueTracker(정보 참고 https://jm4488.tistory.com/29)
+ - Red Mine
 
 Git
  - GitHub
 
 IDE
- - Atom
-
-
-
-
-
+ - Atom/Eclipse
 
 
 
--- vue-now-ui-kit-master/src/pages/Article.vue
+++ vue-now-ui-kit-master/src/pages/Article.vue
@@ -7,8 +7,8 @@
       >
       </parallax>-->
 
-      <div class="container">
-        <!--<div class="content-center brand">
+      <!--<div class="container">
+        <div class="content-center brand">
           <img class="n-logo" src="img/now-logo.png" alt="" />
           <h1 class="h1-seo">Now UI Kit.</h1>
           <h3>A beautiful Bootstrap 4 UI kit. Yours free.</h3>
@@ -24,8 +24,8 @@
               class="creative-tim-logo"
             /> </a
           >.
-        </h6>-->
-      </div>
+        </h6>
+      </div>-->
     </div>
     <div class="main">
       <!--<div class="section section-images">
@@ -46,33 +46,10 @@
         </div>
       </div>-->
     </div>
-    <basic-elements></basic-elements>
     <navigation></navigation>
-    <tabs-section></tabs-section>
-    <progress-pagination></progress-pagination>
-    <notifications></notifications>
     <typography></typography>
-    <javascript-components></javascript-components>
-    <carousel-section></carousel-section>
-    <nucleo-icons-section></nucleo-icons-section>
-    <div class="section">
-      <div class="container text-center">
-        <div class="row justify-content-md-center">
-          <div class="col-md-12 col-lg-8">
-            <h2 class="title">Completed with examples</h2>
-            <h5 class="description">
-              The kit comes with three pre-built pages to help you get started
-              faster. You can change the text and images and you're good to go.
-              More importantly, looking at them will give you a picture of what
-              you can built with this powerful Bootstrap 4 ui kit.
-            </h5>
-          </div>
-        </div>
-      </div>
-    </div>
-    <signup-form></signup-form>
-    <examples-section></examples-section>
-    <download-section></download-section>
+
+
   </div>
 </template>
 <script>
